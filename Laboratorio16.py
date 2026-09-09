9fe3ff5008dc79f9cd4b75d89e476e4dea91308f
Student <student@example.com>
2026-09-09 15:36:45 +0000

    Uso del depurador

---
+++ Laboratorio16.py
@@ -0,0 +1,6 @@
+#Ejercicio 1: Exploración de las características básicas del depurador Python de AWS Cloud9
+
+name = "John"
+print("Hello " + name + ".")
+age = 40
+print(name + " is " + str(age) + " years old.")
